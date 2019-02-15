7174e565cb36ddc1277c1295f12e8b2899f489d3
Steve Golden <sgolden2525@outlook.com>
2019-02-15 12:18:45 -0600

    Add route
    Added get method with express and send/sent text message 'Hello world').

--- customerapp/app.js
+++ customerapp/app.js
@@ -4,6 +4,12 @@ var path = require('path');
 
 var app = express();
 
+/* route */
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+/* listen */
 app.listen(3000, function () {
   console.log("Server started on Port 3000....")
 })
